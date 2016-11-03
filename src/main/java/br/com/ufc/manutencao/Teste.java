49c78d5473455d98b49b1d49b6f1898d90ba70d9
Emerson Vieira <mensones.1@gmail.com>
2016-11-03 00:56:26 -0300

    Fix error on issue #1

--- src/main/java/br/com/ufc/manutencao/Teste.java
+++ src/main/java/br/com/ufc/manutencao/Teste.java
@@ -1,10 +1,11 @@
 package br.com.ufc.manutencao;
 
+//Classe de teste para resolver issue
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Teste de issue");
 	}
 
 }
